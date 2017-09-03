70c4a0ed2f3426dbfc70ba0c9bfc5f3588cce300
jamgar <github email address>
2017-09-03 17:31:53 -0500

    Update ticket model and add role to users

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,7 @@
 class Ticket < ApplicationRecord
+  belongs_to :client_id, class_name: 'User'
+  belongs_to :technician_id, class_name: 'User'
+
   enum status: [:open, :in_progress, :pending, :closed]
 
   validates :subject, presence: true
