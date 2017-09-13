7f748335959f9de7cb44a29ed98f1959468b16a9
jamgar <jegpub@gmail.com>
2017-09-13 11:25:20 -0500

    Update devise override

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :role
-      devise_parameter_sanitizer.for(:account_update) << :role
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:role, :email, :password) }
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:role, :email, :password, :current_password) }
     end
 end
